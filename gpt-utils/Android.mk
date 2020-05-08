9738e3360f06525a8b3d5473542b0df585ff8001
Mohd Faraz <androiabledroid@gmail.com>
2020-05-09 05:14:17 +0530

    ime: Added required headers
    Signed-off-by: jeanrivera <jeanrivera@gmail.com>

--- gpt-utils/Android.mk
+++ gpt-utils/Android.mk
@@ -2,19 +2,27 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := bootable/recovery \
-		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES := bootable/recovery
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
+ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+else
+LOCAL_HEADER_LIBRARIES := gpt-kernel-headers
+endif
 LOCAL_MODULE := libgptutils
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := bootable/recovery \
-		    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES := bootable/recovery
 LOCAL_SRC_FILES := gpt-utils.cpp sparse_crc32.cpp
+ifeq ($(TARGET_COMPILE_WITH_MSM_KERNEL),true)
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+else
+LOCAL_HEADER_LIBRARIES := gpt-kernel-headers
+endif
 LOCAL_SHARED_LIBRARIES += liblog libcutils
 LOCAL_MODULE := libgptutils
 LOCAL_COPY_HEADERS_TO := gpt-utils/inc
